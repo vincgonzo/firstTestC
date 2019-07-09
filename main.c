6f9a61ff8a94937a4e9f92e79b81383c148cf44b
vincgonzo <vincgonzo@hotmail.fr>
2019-07-09 19:10:24 +0200

    average calculation

--- main.c
+++ main.c
@@ -3,12 +3,14 @@
 #include <time.h>
 
 int sommeTableau(int tableau[], int tailleTableau);
+double moyenneTableau(int tableau[], int tailleTableau);
 
 int main()
 {
     int tableau[4] = {10, 20, 15};
 
     sommeTableau(tableau, 4);
+    moyenneTableau(tableau, 4);
 
     return 0;
 }
@@ -19,5 +21,21 @@ int sommeTableau(int tableau[], int tailleTableau)
     for(i=0; i < tailleTableau; i++){
         somme += tableau[i];
     }
-    printf("somme du tableau = %d", somme);
+    printf("somme du tableau = %d.\n", somme);
+}
+
+double moyenneTableau(int array[], int tailleTableau)
+{
+    float avg;
+    int sum = 0, loop = 0;
+
+    for(loop = 0; loop < tailleTableau; loop++) {
+            printf("\nsum == %d", sum);
+          sum = sum + array[loop];
+    }
+
+    avg = (float)sum / tailleTableau;
+    printf("\nAverage of array values is %.2f", avg);
+
+   return 0;
 }
