a299d65522e8a6514180cc6e6db58a56151ba65e
vincgonzo <vincgonzo@hotmail.fr>
2019-07-11 14:08:55 +0200

    adding file reading limitation for test

--- main.c
+++ main.c
@@ -3,12 +3,14 @@
 #include <time.h>
 #include <string.h>
 #include "main.h"
+#define TAILLE_MAX 10 // define max value
 
 
 int main(int argc, char *argv[])
 {
     FILE* fichier = NULL;
     int read = 0, caractereActuel = 0;
+    char string[TAILLE_MAX] = "";
 
     fichier = fopen("test.txt", "r+");
 
@@ -19,12 +21,16 @@ int main(int argc, char *argv[])
         printf("Voulez vous lire le contenu du fichier ?\n1 -- Oui\n0 -- Non\n");
         scanf("%d", &read);
         if(read){
-            // Boucle de lecture des caractères un à un
-            do
+            // loop to read every char of a file
+            /*do
             {
                 caractereActuel = fgetc(fichier); // On lit le caractère
                 printf("%c", caractereActuel); // On l'affiche
             } while (caractereActuel != EOF); // On continue tant que fgetc n'a pas retourné EOF (fin de fichier)
+            */
+            //  string read with limited size
+            fgets(string, TAILLE_MAX, fichier);
+            printf("%s", string);
 
         }
         fclose(fichier);
