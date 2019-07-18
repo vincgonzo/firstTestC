1183643e0940891bcf473ac78a03db2acbf116ad
vincgonzo <vincgonzo@hotmail.fr>
2019-07-18 12:31:49 +0200

    get int, double

--- main.c
+++ main.c
@@ -5,14 +5,26 @@
 #include "main.h"
 
 void viderBuffer();
+long lireLong();
+double lireDouble();
 int lire(char *chaine, int longueur);
 
 int main(){
     char nom[10];
+    long age = 0;
+    double poids = 0;
 
-    printf("Quel est votre nom ? ");
+    /*printf("Quel est votre nom ? ");
     lire(nom, 10);
     printf("Ah ! Vous vous appelez donc %s !\n\n", nom);
+
+    printf("Quel est votre age ? ");
+    age = lireLong();
+    printf("Ah ! Vous avez donc %d ans !\n\n", age);*/
+
+    printf("Quel est votre poids ? ");
+    poids = lireDouble();
+    printf("Tu peses donc %d kg !\n\n", poids);
 
     return 0;
 }
@@ -47,6 +59,39 @@ int lire(char *chaine, int longueur)
     else
     {
         viderBuffer();
+        return 0;
+    }
+}
+
+long lireLong()
+{
+    char nombreTexte[100] = {0}; // 100 cases devraient suffire
+
+    if (lire(nombreTexte, 100))
+    {
+        // Si lecture du texte ok, convertir le nombre en long et le retourner
+        return strtol(nombreTexte, NULL, 10);
+    }
+    else
+    {
+        // Si problème de lecture, renvoyer 0
+        return 0;
+    }
+}
+
+double lireDouble()
+{
+     char nbrTxt[100] = {0}; // 100 cases devraient suffire
+
+    if (lire(nbrTxt, 100))
+    {
+        printf('%s', nbrTxt);
+        // Si lecture du texte ok, convertir le nombre en long et le retourner
+        return strtod(nbrTxt, NULL);
+    }
+    else
+    {
+        // Si problème de lecture, renvoyer 0
         return 0;
     }
 }
