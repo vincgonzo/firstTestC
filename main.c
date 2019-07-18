6a3f25e8560894bc4249dd974189caf09a13ff5f
vincgonzo <vincgonzo@hotmail.fr>
2019-07-18 12:05:17 +0200

    get text and carreful with buffer overflow

--- main.c
+++ main.c
@@ -3,42 +3,50 @@
 #include <time.h>
 #include <string.h>
 #include "main.h"
-#include "coord.h"
 
+void viderBuffer();
+int lire(char *chaine, int longueur);
 
 int main(){
-    char text[100], string[] = "Bouzouff";
-    int StringLength = 0;
-    char chaine[] = "Texte de test", *suiteChaine = NULL;
+    char nom[10];
 
-    Coordonnees monPoint;
-
-    initialiserCoordonnees(&monPoint);
-
-    printf("We got a Coordinates Struct with x : %d -- and y : %d.\n", point.x, point.y);
-
-    printf("Comment t'appeles tu ? : ");
-    scanf("%s", text);
-    StringLength = strlen(text);
-    printf("Bienvenue %s, on va tester des trucs ensemble :)\nCette chaine de caractère fait %d de longueur.", text, StringLength);
-
-    strcat(text, string);
-    printf("\n\nPour la deconnade je dis %s\n", text);
-
-
-    suiteChaine = strchr(chaine, 'd');
-    if (suiteChaine != NULL) // Si on a trouvé quelque chose
-    {
-        printf("Voici la fin de la chaine a partir du premier d : %s", suiteChaine);
-    }
+    printf("Quel est votre nom ? ");
+    lire(nom, 10);
+    printf("Ah ! Vous vous appelez donc %s !\n\n", nom);
 
     return 0;
 }
 
-void initialiserCoordonnees(Coordonnees* point)
+
+void viderBuffer()
 {
-    point.x = 10;
-    point.y = 25;
+    int c = 0;
+    while (c != '\n' && c != EOF)
+    {
+        c = getchar();
+    }
 }
 
+int lire(char *chaine, int longueur)
+{
+    char *positionEntree = NULL;
 
+    if (fgets(chaine, longueur, stdin) != NULL)
+    {
+        positionEntree = strchr(chaine, '\n');
+        if (positionEntree != NULL)
+        {
+            *positionEntree = '\0';
+        }
+        else
+        {
+            viderBuffer();
+        }
+        return 1;
+    }
+    else
+    {
+        viderBuffer();
+        return 0;
+    }
+}
